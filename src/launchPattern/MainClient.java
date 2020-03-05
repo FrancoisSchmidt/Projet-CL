737b6e7c93de094e6ba30975a68866a24642e931
Franz57 <francois.schmidt@outlook.com>
2020-03-05 19:37:27 +0100

    Ajout deconnexion dans ProtocoleLogin

--- src/launchPattern/MainClient.java
+++ src/launchPattern/MainClient.java
@@ -6,12 +6,12 @@ public class MainClient {
 
 	public static void main(String[] args) {
 		ClientTCP myClt = new ClientTCP("localhost", 6666 );
-		
+
 		if ( myClt.connecterAuServeur() ) {
 			myClt.transmettreChaine("PING");
 			//myClt.deconnecterDuServeur();
 		}
-	
+
 	}
 
 }
