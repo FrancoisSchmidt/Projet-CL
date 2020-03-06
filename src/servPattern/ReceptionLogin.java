dbab945dd45c6314558262967f62e1c253f1c5bc
Elie NGUYEN-HUY-THUY <elie.nguyen-huy-thuy@ensta-bretagne.org>
2020-03-06 08:55:10 +0100

    ReceptionLogin2

--- src/servPattern/ReceptionLogin.java
+++ src/servPattern/ReceptionLogin.java
@@ -2,26 +2,40 @@ package servPattern;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+
 public class ReceptionLogin {
+
     private Socket loginSocket;
+
     private PrintStream socOut;
+
     private BufferedReader socIn;
+
     private int connexion = 0;
+
     private int maxConnexions = 1;
+
     private int numeroPort;
 
-    public ReceptionLogin(int unNumeroPort) {
+    private String nomServeur;
+
+    public ReceptionLogin(int unNumeroPort, String unnomServeur) {
         numeroPort = unNumeroPort;
+        nomServeur = unnomServeur;
     }
 
     public void run() {
         ServerSocket serverSocket = null;
         try {
-            serverSocket = new ServerSocket(numeroPort);
+            loginSocket = new Socket(nomServeur, numeroPort);
+            socOut = new PrintStream(loginSocket.getOutputStream());
+            socIn = new BufferedReader (
+                    new InputStreamReader(loginSocket.getInputStream()));
         } catch (IOException e) {
             System.out.println("Could not listen on port: " + numeroPort + ", " + e);
             System.exit(1);
@@ -36,7 +50,11 @@ public class ReceptionLogin {
         }
 
         try {
-            socIn.readLine();
+            String nom = null;
+            String mdp = null;
+            nom = socIn.readLine();
+            mdp = socIn.readLine();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
