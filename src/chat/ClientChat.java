0f634ed6a3031439ab1e6c7d1aba680409c4dd0c
elien <elie.nguyen-huy-thuy@ensta-bretagne.org>
2020-04-08 15:23:44 +0200

    ProtocoleChat MàJ

--- src/chat/ClientChat.java
+++ src/chat/ClientChat.java
@@ -4,6 +4,8 @@ import java.io.*;
 import java.net.*;
 
 public class ClientChat {
+    private String nmcl;
+
     private int numeroPort;
 
     private String nomServeur;
@@ -18,17 +20,19 @@ public class ClientChat {
 
     private String groupe_cible;
 
-    public ClientChat(String unNomServeur, int unNumero, String msg, String user){
+    public ClientChat(String unNomServeur, int unNumero, String msg, String user, String nom_client){
         numeroPort = unNumero;
         nomServeur = unNomServeur;
         message = msg;
         groupe_cible = user;
+        nmcl = nom_client;
     }
 
     public String transmettreMessage(PrintStream socOut, BufferedReader socIn) {
         String msgServeur = null;
         try {
             System.out.println("requete client : envoie de " + this.message + " à " + this.groupe_cible);
+            socOut.println(this.nmcl);
             socOut.println(this.groupe_cible);
             socOut.println(this.message);
             socOut.flush();
--- src/chat/ProtocoleChat.java
+++ src/chat/ProtocoleChat.java
@@ -1,4 +1,33 @@
 package chat;
 
-public class ProtocoleChat {
+import servPattern.IContext;
+import servPattern.IProtocole;
+import context.LeContext;
+import java.io.*;
+
+import login.ToolsIds;
+import servPattern.IProtocole;
+
+public class ProtocoleChat implements IProtocole {
+
+    private ToolsIds Ids = new ToolsIds();
+    private String name = "";
+
+    public void execute(IContext c , InputStream unInput , OutputStream unOutput ){}
+    public String getName(){return "";}
+    public void executebis(IContext LeContext, BufferedReader is, PrintStream os){
+        String inputReq;
+        try{
+            String valeurExpediee="";
+            if ((inputReq=is.readLine()) != null) {
+                System.out.println("Ordre Recu_login" + inputReq);
+                String grp_cible = is.readLine();
+                String msg = is.readLine();
+                System.out.println(grp_cible + ": " + msg);
+                LeContext.sendMessageToAll(inputReq, msg);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
